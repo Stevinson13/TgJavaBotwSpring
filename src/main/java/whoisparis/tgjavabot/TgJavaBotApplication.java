8140fc37cd6c5c42ebff87f7f16351ac773ab3f8
whoisparis <stevinplay@gmail.com>
2023-04-26 19:31:41 +0300

    Telegram bot

---
+++ src/main/java/whoisparis/tgjavabot/TgJavaBotApplication.java
@@ -0,0 +1,14 @@
+package whoisparis.tgjavabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgJavaBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                TgJavaBotApplication.class, args);
+    }
+
+}
